33455730d396d06aa85ed3333a2455ca9c07d7bc
Adarsh <adarsh9540984202@gmail.com>
2024-03-22 23:57:13 +0530

    add it

--- middleware/admin.js
+++ middleware/admin.js
@@ -15,7 +15,7 @@ function adminMiddleware (req,res,next){
             next();
         }else{
             res.status(403).json({
-                msg:"user donesnot exist"
+                msg:"Admin donesnot exist"
             })
         }
     })
